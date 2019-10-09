8580bf8c9711f9a94264ff3d261a26f143c96d3c
vladster95 <V.L.1655@gmail.com>
2019-10-09 19:25:38 +0300

    init: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+    let val = BigInt(1);
+    let pos = -1;
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] !== '!') {
+            pos < 0 ? pos = i : null;
+            i++;
+            if (expression[i] === '!') {
+                let step = 1;
+                expression[i + 1] === '!' ? step = 2 : null;
+                for (let j = Number(expression.substring(pos, i)); j > 1; j -= step) {
+                    val = BigInt(val) * BigInt(j);
+                }
+                pos = -1;
+                i += step;
+            } else {
+                i--;
+            }
+        }
+    }
+    expression = val.toString();
+    val = 0;
+    for (let k = 1; k < expression.length; k++) {
+        if (Number(expression[expression.length - k])) {
+            break;
+        } else {
+            val++;
+        }
+    }
+    return val;
 }
